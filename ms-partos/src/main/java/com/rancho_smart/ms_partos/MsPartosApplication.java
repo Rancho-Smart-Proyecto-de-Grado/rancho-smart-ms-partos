c18edd78405620549554bba11fbbcae65dddaff6
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:02:13 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-partos/src/main/java/com/rancho_smart/ms_partos/MsPartosApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_partos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPartosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPartosApplication.class, args);
+	}
+
+}
---
+++ ms-partos/src/test/java/com/rancho_smart/ms_partos/MsPartosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_partos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPartosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
