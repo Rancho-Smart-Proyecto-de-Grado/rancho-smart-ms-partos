773570b079e849745aa02d93da2dff9938f58b52
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-13 06:59:46 -0500

    :wrench: chore: Configuration Server + DB Creation + Environment Variables + Eureka Config.

--- src/main/java/com/rancho_smart/ms_partos/MsPartosApplication.java
+++ src/main/java/com/rancho_smart/ms_partos/MsPartosApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_partos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPartosApplication {
 
 	public static void main(String[] args) {
